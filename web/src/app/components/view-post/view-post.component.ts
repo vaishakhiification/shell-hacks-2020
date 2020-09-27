700063068229eb155b9f4b5d4029e6b100450223
anjanakvellore <anjanavellore@gmail.com>
2020-09-26 22:47:29 -0700

    Checking in html structure for view-post component and respond component

--- web/src/app/components/view-post/view-post.component.ts
+++ web/src/app/components/view-post/view-post.component.ts
@@ -1,15 +1,22 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import {NgbModal, ModalDismissReasons, NgbModalOptions} from '@ng-bootstrap/ng-bootstrap';
 
 @Component({
-  selector: 'app-view-post',
-  templateUrl: './view-post.component.html',
-  styleUrls: ['./view-post.component.css']
+  selector: 'app-view-post',
+  templateUrl: './view-post.component.html',
+  styleUrls: ['./view-post.component.css']
 })
-export class ViewPostComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
+export class ViewPostComponent implements OnInit {
+  modalOptions: NgbModalOptions;
+  constructor(private modalService: NgbModal) { }
+  ngOnInit(): void {
+  }
+  open(content) {
+    this.modalService.open(content, this.modalOptions).result.then((result) => {
+    //   this.closeResult = `Closed with: ${result}`;
+    //   this.bookAppointment();
+    // }, (reason) => {
+    //   this.closeResult = `Dismissed ${this.getDismissReason(reason)}`;
+    });
+  }
 }
