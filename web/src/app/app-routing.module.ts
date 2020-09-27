9682c82ce11c6370fa29a9126fe015356966d1bb
vaishakhi_pilankar <vaishakhipilankar@gmail.com>
2020-09-26 22:07:33 -0700

    Error fixes

--- web/src/app/app-routing.module.ts
+++ web/src/app/app-routing.module.ts
@@ -1,10 +1,10 @@
-import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
+import {NgModule} from '@angular/core';
+import {Routes, RouterModule} from '@angular/router';
 import {LoginComponent} from './components/login/login.component';
-import {SearchComponent} from "./components/search/search.component";
-import { CreatePostComponent } from './components/create-post/create-post.component';
-import { ViewPostComponent } from './components/view-post/view-post.component';
-import { RespondPostComponent } from './components/respond-post/respond-post.component';
+import {SearchComponent} from './components/search/search.component';
+import {CreatePostComponent} from './components/create-post/create-post.component';
+import {ViewPostComponent} from './components/view-post/view-post.component';
+import {RespondPostComponent} from './components/respond-post/respond-post.component';
 
 const routes: Routes = [
   {path: '', component: LoginComponent},
@@ -19,4 +19,5 @@ const routes: Routes = [
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {
+}
--- web/src/app/app.module.ts
+++ web/src/app/app.module.ts
@@ -6,15 +6,15 @@ import {AngularFireAuthModule} from '@angular/fire/auth';
 import {AngularFireStorageModule} from '@angular/fire/storage';
 import {AngularFirestoreModule} from '@angular/fire/firestore';
 import {environment} from '../environments/environment';
-import { LoginComponent } from './components/login/login.component';
-import { SearchComponent } from './components/search/search.component';
+import {LoginComponent} from './components/login/login.component';
+import {SearchComponent} from './components/search/search.component';
 import {AppRoutingModule} from './app-routing.module';
-import {FormsModule} from "@angular/forms";
-import {HttpClientModule} from "@angular/common/http";
-import { CreatePostComponent } from './components/create-post/create-post.component';
-import { ViewPostComponent } from './components/view-post/view-post.component';
-import { RespondPostComponent } from './components/respond-post/respond-post.component';
-import { FontAwesomeModule } from '@fortawesome/angular-fontawesome'
+import {FormsModule} from '@angular/forms';
+import {HttpClientModule} from '@angular/common/http';
+import {CreatePostComponent} from './components/create-post/create-post.component';
+import {ViewPostComponent} from './components/view-post/view-post.component';
+import {RespondPostComponent} from './components/respond-post/respond-post.component';
+import {FontAwesomeModule} from '@fortawesome/angular-fontawesome';
 import {AgmCoreModule} from '@agm/core';
 
 @NgModule({
@@ -34,7 +34,7 @@ import {AgmCoreModule} from '@agm/core';
     AngularFirestoreModule,
     AgmCoreModule.forRoot({
       apiKey: 'AIzaSyCdi36DnJlcWtZyQVWQ0z9EJThIJquoYFI'
-    })
+    }),
     AppRoutingModule,
     FormsModule
   ],
