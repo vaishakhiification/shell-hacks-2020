c10759461c4a1522be07fe59327f2132cdf54f24
vaishakhi_pilankar <vaishakhipilankar@gmail.com>
2020-09-26 22:01:47 -0700

    Google Maps + utils

--- web/src/app/app.module.ts
+++ web/src/app/app.module.ts
@@ -1,11 +1,12 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
-import { AppComponent } from './app.component';
-import { AngularFireModule } from '@angular/fire';
-import { AngularFireAuthModule } from '@angular/fire/auth';
-import { AngularFireStorageModule } from '@angular/fire/storage';
-import { AngularFirestoreModule } from '@angular/fire/firestore';
+import {BrowserModule} from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
+import {AppComponent} from './app.component';
+import {AngularFireModule} from '@angular/fire';
+import {AngularFireAuthModule} from '@angular/fire/auth';
+import {AngularFireStorageModule} from '@angular/fire/storage';
+import {AngularFirestoreModule} from '@angular/fire/firestore';
 import {environment} from '../environments/environment';
+import {AgmCoreModule} from '@agm/core';
 
 @NgModule({
   declarations: [
@@ -17,8 +18,12 @@ import {environment} from '../environments/environment';
     AngularFireAuthModule,
     AngularFireStorageModule,
     AngularFirestoreModule,
+    AgmCoreModule.forRoot({
+      apiKey: 'AIzaSyCdi36DnJlcWtZyQVWQ0z9EJThIJquoYFI'
+    })
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
---
+++ web/src/app/utils/utilities.ts
@@ -0,0 +1,41 @@
+// :::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
+// :::                                                                         :::
+// :::  This routine calculates the distance between two points (given the     :::
+// :::  latitude/longitude of those points). It is being used to calculate     :::
+// :::  the distance between two locations using GeoDataSource (TM) prodducts  :::
+// :::                                                                         :::
+// :::  Definitions:                                                           :::
+// :::    South latitudes are negative, east longitudes are positive           :::
+// :::                                                                         :::
+// :::  Passed to function:                                                    :::
+// :::    lat1, lon1 = Latitude and Longitude of point 1 (in decimal degrees)  :::
+// :::    lat2, lon2 = Latitude and Longitude of point 2 (in decimal degrees)  :::
+// :::    unit = the unit you desire for results                               :::
+// :::           where: 'M' is statute miles (default)                         :::
+// :::                  'K' is kilometers                                      :::
+// :::                  'N' is nautical miles                                  :::
+// :::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
+export function calculateDistance(lat1, lon1, lat2, lon2, unit): number {
+  if ((lat1 === lat2) && (lon1 === lon2)) {
+    return 0;
+  } else {
+    const radLat1 = Math.PI * lat1 / 180;
+    const radLat2 = Math.PI * lat2 / 180;
+    const theta = lon1 - lon2;
+    const radTheta = Math.PI * theta / 180;
+    let distance = Math.sin(radLat1) * Math.sin(radLat2) + Math.cos(radLat1) * Math.cos(radLat2) * Math.cos(radTheta);
+    if (distance > 1) {
+      distance = 1;
+    }
+    distance = Math.acos(distance);
+    distance = distance * 180 / Math.PI;
+    distance = distance * 60 * 1.1515;
+    if (unit === 'K') {
+      distance = distance * 1.609344;
+    }
+    if (unit === 'N') {
+      distance = distance * 0.8684;
+    }
+    return distance;
+  }
+}
