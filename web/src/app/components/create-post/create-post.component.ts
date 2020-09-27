c744fdde47541ca8c8aace9f0631ccf19e802e7e
anjanakvellore <anjanavellore@gmail.com>
2020-09-27 03:47:30 -0700

    Maintaining the username of the logged in user throughout the session and removing some commented code

--- web/src/app/components/create-post/create-post.component.ts
+++ web/src/app/components/create-post/create-post.component.ts
@@ -3,6 +3,7 @@ import { Post } from '../../models/index';
 import {AgmMarker} from '@agm/core';
 import { DataService } from 'src/app/services/data.service';
 import { ConstantService } from 'src/app/services/constant.service';
+import {ActivatedRoute, Router} from "@angular/router";
 
 @Component({
   selector: 'app-create-post',
@@ -16,7 +17,8 @@ export class CreatePostComponent implements OnInit {
     latitude: 51.678418,
     longitude: 7.809007,
   };
-  constructor(private dataService: DataService, private constantService: ConstantService) { 
+  constructor(private dataService: DataService, private constantService: ConstantService,
+    private router: Router) { 
     this._getCurrentLocation();
   }
   ngOnInit(): void {
@@ -42,14 +44,14 @@ export class CreatePostComponent implements OnInit {
     this.newPost.authorApproved = false;
     this.newPost.responderApproved = false;
     this.newPost.postStatusId = "1";
-    this.newPost.authorName = this.constantService.currentUser.userName;
+    this.newPost.authorName = this.constantService.currentUser;
     this.newPost.latitude = this.position.latitude;
     this.newPost.longitude = this.position.longitude;
     this.dataService.createPost(this.newPost);
   }
 
   navigateBack(){
-    
+    this.router.navigate(['/search']);
   }
 
 
